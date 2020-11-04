c92a1350329bddc9fb116b20170d15206dbdedc0
Ashish Neupane <aasesshenry@gmail.com>
2020-11-04 10:19:09 +0545

    models built is starting

---
+++ API/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+#unique and randon sku generator
+from .utils import unique_sku_generator 
+from django.db.models.signals import pre_save
+
+# Create your models here.
+
+class Vendor(models.Model):
+    name = models.CharField(max_length = 200,blank=True,null=True) 
+    address = models.CharField(max_length = 200,blank=True,null=True)
+    phone = models.CharField(max_length = 12,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.CharField(max_length = 200,blank=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    category_choice = [
+        ['Medicine','Medicine'],
+        ['Paste','Paste'],
+        ['Brush','Brush']
+    ]
+
+    stock_choices = (
+        (0,'Out of Stock'),
+        (1,'In Stock')
+    )
+
+    sku = models.CharField(max_length=15, blank= True)
+    item_name = models.CharField(max_length=200)
+    category = models.CharField(choices = category_choice,max_length = 200,blank=True,null=True)
+    price = models.FloatField(default=0)
+    vendor = models.ForeignKey(
+        Vendor, #one vendor has many items
+        on_delete = models.CASCADE,
+        related_name = "items"
+    )
+    remaining_quantity = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.CharField(max_length = 200,blank=True)
+    is_stock = models.IntegerField(choices = stock_choices)
+
+    def __str__(self):
+        return self.name
+
+    def check_instock(self):
+        if(self.remaining_quantity > 0 ):
+            return True
+
+class Stock(models.Model):
+    item = models.ForeignKey(
+        Item, #one item has many stocks left
+        on_delete = models.CASCADE,related_name="stocks"
+        )
+    recieved_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    recieved_by = models.CharField(max_length = 200)
+    updated_by = models.CharField(max_length = 200,blank=True)
+    recieved_quantity = models.IntegerField(default=0)
+
+   
+
+class Sale(models.Model):
+    item = models.ForeignKey(
+        Item, #one item is sold to many
+        on_delete = models.CASCADE,related_name="sales"
+        )
+    sold_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    sold_by = models.CharField(max_length = 200)
+    updated_by = models.CharField(max_length = 200,blank=True)
+    sold_quantity = models.IntegerField(default=0)
+
+
+def pre_save_create_new_sku(sender, instance, *args, **kwargs):
+    if not instance.sku:
+        instance.sku= unique_sku_generator(instance)
+
+pre_save.connect(pre_save_create_new_sku, sender=Item)
+
---
+++ API/utils.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+#function to generate random numbers.
+def random_sku_generator(size=10,chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def unique_sku_generator(instance):
+    new_random_sku = random_sku_generator()
+    print(new_random_sku)
+    #instance of item class
+    Kclass= instance.__class__
+    #check if that sku already exists
+    query_exists= Kclass.objects.filter(sku= new_random_sku).exists()
+    #if exist call the function again
+    if query_exists:
+        return unique_sku_generator(instance)
+    
+    return new_random_sku
