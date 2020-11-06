137c0c933f48b3ed15f5fc9d074e9904808a91e6
diwasx <diwasx@gmail.com>
2020-11-06 22:35:07 +0545

    search feature

--- API/admin.py
+++ API/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Vendor,Item,Stock
+from .models import Vendor, Item, Stock, Sale
 # Register your models here.
 admin.site.register(Vendor)
-admin.site.register(Item)
+# admin.site.register(Item)
 admin.site.register(Stock)
+admin.site.register(Sale)
+
+class item_search(admin.ModelAdmin):
+    search_fields = ['item_name','price']
+
+admin.site.register(Item, item_search)
+
+
+
