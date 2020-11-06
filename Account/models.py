57dd2822732118290eb4439d30fc574f9fead19b
Ashish Neupane <aasesshenry@gmail.com>
2020-11-06 10:11:28 +0545

    password hashed for employee and employee model changed--removed username and made phone a unique column

--- Account/models.py
+++ Account/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class Employee(models.Model):
     first_name = models.CharField(max_length = 200)
     last_name = models.CharField(max_length = 200)
-    username = models.CharField(max_length = 200)
-    phone = models.CharField(max_length = 15)
+    phone = models.CharField(max_length = 15,unique=True)
     address = models.CharField(max_length = 200)
     password = models.CharField(max_length = 200,blank=True)
 
--- Account/views.py
+++ Account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from .models import Employee
+from django.contrib.auth.hashers import make_password
+
 # Create your views here.
 def signup(request):
     if(request.method == "POST"):
@@ -8,11 +10,20 @@ def signup(request):
         if form.is_valid():
             #get first phone number
             phone = int(request.POST.dict()['phone'])
+            password = request.POST.dict()['password1']
             #check if that number is registered or not
             employee = Employee.objects.filter(phone=phone)
             if employee.exists():
-                form.save()
-                return redirect('login')
+                #check if initial password is empty or not
+                for emp in employee:
+                    if(emp.password == ""):   
+                        form.save()
+                        #hash the password and save it to employee
+                        emp.password = make_password(password = emp.password,salt=None,hasher='default')
+                        emp.save()
+                        return redirect('login')
+                    else:
+                        return render(request,'Account/Signup.html',{'form':form,'error_phone':"User is already registered with same phone number."})
             else:
                 return render(request,'Account/Signup.html',{'form':form,'error_phone':"Phone Number is not registered."})
             
