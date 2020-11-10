7dc4217c7b60c4a4cb0bc50cd870e34a05ceee78
Ashish Neupane <aasesshenry@gmail.com>
2020-11-10 23:06:14 +0545

    json file added... create a new json file in your base dir

--- Frontend/token_generator.py
+++ Frontend/token_generator.py
@@ -1,11 +1,15 @@
 import requests
-
+import json
+import os
+from StockManagementSystem.settings import BASE_DIR
 
 def access_token_generate():
-    data = {
-        'username':'ashish',
-        'password':'1234'
-    }
+    jsonpath = os.path.join(BASE_DIR,'data.json')
+    with open(jsonpath) as f:
+        data = json.load(f)
+        print(data)
+
+
     session = requests.Session()
     session.trust_env = False
     res = requests.post(url='http://127.0.0.1:8000/api/gettoken/',data=data)
