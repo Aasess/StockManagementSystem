ffa90a7486e531efac3923bea4dacce1712ed88e
Diwash Ale <diwasx@gmail.com>
2020-11-13 05:29:47 +0000

    time bug fixed

--- Frontend/static/js/app.js
+++ Frontend/static/js/app.js
@@ -1,77 +1,3 @@
-// const itemoutput = document.querySelector("tbody");
-
-// //funtions
-
-// function getToken(){
-//     return new Promise((resolve)=> {
-//         // fetch('http://127.0.0.1:8000/api/gettoken/',{
-//         fetch('http://localhost:8000/api/gettoken/',{
-//         method: 'POST',
-//         headers: {
-//             'Accept':'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             'username': 'yourname',
-//             'password':'yourpassword'
-//         })
-//     })
-//     .then((res) => res.json())
-//     .then((data)=> {
-//         resolve(data['access'])
-//     })
-//     })
-// }
-
-// function getItem(token){
-//     var bearer = 'Bearer ' + token;
-//     // fetch('http://127.0.0.1:8000/api/item/',{
-//     fetch('http://localhost:8000/api/item/',{
-//         method: 'GET',
-//         withCredentials: true,
-//         credentials: 'include',
-//         headers: {
-//             'Authorization': bearer,
-//             //'Content-Type': 'application/json'
-//         }
-
-//     })
-//     .then((response)=> response.json())
-//     .then((data) => {
-//         //itemoutput.innerText = "";
-//         data['data'].forEach((object)=>{
-//             itemoutput.innerHTML += `
-//             <tr>
-//                 <td>${ moment(new Date(object.created_at)).format("MMM.DD,YYYY, hh:MM A") }</td>
-//                 <td>${ object.sku }</td>
-//                 <td>${ object.item_name }</td>
-//                 <td>${ object.category_detail.category_name }</td>
-//                 <td>${ object.price }</td>
-//                 <td>${ object.vendor_detail.name }</td>
-//                 <td class="pl-5">${ object.remaining_quantity }</td>
-//                 <td id="is_stock">${ object.is_stock }</td>
-//             </tr>
-//         `;
-//         })
-//         const stock_value = document.querySelectorAll("#is_stock");
-//         stock_value.forEach((d)=>{
-//             if(d.innerText == "true"){
-//                 d.innerHTML = `<img src ="http://127.0.0.1:8000/static/admin/img/icon-yes.svg">`;
-//             }
-//             else{
-//                 d.innerHTML = `<img src ="http://127.0.0.1:8000/static/admin/img/icon-no.svg">`;
-//             }
-//         });
-//     })
-//     .then(function() {
-//         $('#myTable').DataTable({
-//             order: [0, 'desc'],
-//         });
-//     })
-
-// }
-//getToken().then(getItem);
-
 $('#myTable').DataTable({
     order: [0, 'desc'],
 });
@@ -97,5 +23,5 @@ init();
 
 dateall = document.querySelectorAll("#date")
 dateall.forEach((date)=>{
-    date.innerText = moment(new Date(date.innerText)).format("YYYY-MM-DD, hh:MM A")
+    date.innerText = moment(new Date(date.innerText)).format("YYYY-MM-DD, hh:mm A")
 })
